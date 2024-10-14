1c68e3836dd616569e704c18f0c9ceabe4395b3a
Leonardo Barcaroli <leonardo@koyeb.com>
2024-10-14 15:58:58 +0200

    force building package

--- setup.py
+++ setup.py
@@ -7,9 +7,15 @@ with open("./src/static/test_env.txt", "w") as f:
 with open("./src/static/test_secret.txt", "w") as f:
     f.write(os.environ.get("TEST_SECRET", ""))
 
-with open("./requirements.txt", "r") as f:
-    requirements_txt = f.read()
-    requirements = requirements_txt.split("\n")
+requirements = [
+    "click==8.1.3",
+    "Flask==2.2.2",
+    "gunicorn==20.1.0",
+    "itsdangerous==2.1.2",
+    "Jinja2==3.1.2",
+    "MarkupSafe==2.1.1",
+    "Werkzeug==2.2.2",
+]
 
 setup(
     name="test-e2e-buildpack",
